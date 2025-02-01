22b9c4e85eda435e0d42a6d60697fc029218f86f
Gary Tou <gary@garytou.com>
2025-02-01 01:22:40 -0800

    directly override URL

--- mirror.go
+++ mirror.go
@@ -7,13 +7,7 @@ import (
 )
 
 func setRequestPath(req *http.Request, target *url.URL) {
-	// Set URL. Inspired by ProxyRequest.SetURL
-	req.URL.Scheme = target.Scheme
-	req.URL.Host = target.Host
-	// ProxyRequest.SetURL merges Path, RawPath, and queries, but i don't
-	req.URL.Path = target.Path
-	req.URL.RawPath = target.RawPath
-	req.URL.RawQuery = target.RawQuery
+	req.URL = target
 	req.Host = ""
 }
 
